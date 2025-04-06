31dcab16532e6f932010ba8e73756d7276148f77
tusharsawant04 <tsawant379@gmail.com>
2025-04-06 13:11:11 +0530

    code push

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const path = require("path");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+module.exports = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     files: ["**/*.ts", "**/*.tsx"],
@@ -18,5 +14,3 @@ const eslintConfig = [
     },
   },
 ];
-
-module.exports = eslintConfig;
